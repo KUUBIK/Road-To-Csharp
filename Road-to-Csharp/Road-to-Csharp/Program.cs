c36cca3c35fdf5b1e93f4e979be751b2733313d1
Robert <Robert@DESKTOP-PCFCJQN>
2021-09-25 12:40:42 +0600

    Started

---
+++ Road-to-Csharp/Road-to-Csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Road_to_Csharp
+{
+    class Program
+    {
+        //Extension String methods
+        static void Main(string[] args)
+        {
+            string h = "Hello ";
+            string w = "World ";
+            Console.WriteLine(h + w);
+            string z = default;
+            z.NewExtReversedStringMethods(ref h, ref w);
+            Console.WriteLine(h + w);
+        }
+    }
+
+
+    static class StringExtension
+    {
+        public static void NewExtReversedStringMethods(this string msg, ref string t, ref string z)
+        {
+            string a = z;
+            z = t;
+            t = a;
+        }
+
+
+    }
+}
